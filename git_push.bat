7eb35913f0805a7821630078b9f2ffbb7d383544
Juhyeon AN <ajh9789@gachon.ac.kr>
2025-04-26 14:49:55 +0900

    add commit push 자동화[C파일 추가[C[D[D[D[D[D[D[D[C[C[C를 위한 sh, bat파일 추가[C

---
+++ git_push.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\LabVIEW_Projects
+"C:\Program Files\Git\bin\bash.exe" git_push.sh
+pause
---
+++ git_push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /c/LabVIEW_Projects
+git add .
+echo "커밋 메시지를 입력하세요:"
+read msg
+git commit -m "$msg"
+git push
